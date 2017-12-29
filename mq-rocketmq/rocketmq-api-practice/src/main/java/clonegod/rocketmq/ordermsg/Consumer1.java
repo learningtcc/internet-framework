c5a84f00707e303a1a471b8c2f7137f7d8f9a0a3
clonegod <asynclife@163.com>
2017-12-29 22:45:50 +0800

    update

--- mq-rocketmq/rocketmq-api-practice/src/main/java/clonegod/rocketmq/ordermsg/Consumer1.java
+++ mq-rocketmq/rocketmq-api-practice/src/main/java/clonegod/rocketmq/ordermsg/Consumer1.java
@@ -25,6 +25,12 @@ public class Consumer1 {
         	// 订阅Topic，可以指定Tag对消息进行二次过滤
 			consumer.subscribe(TOPIC_NAME, "*");
 			
+			// 设置消费端的线程池数量
+			// 消费线程池的最小值：默认10
+			consumer.setConsumeThreadMin(10);
+			// 消费线程池的最大值：默认20
+			consumer.setConsumeThreadMax(20);
+
 			// 注册消息处理器
 			consumer.registerMessageListener(new Listener());
 			
@@ -39,10 +45,11 @@ public class Consumer1 {
 	/**
 	 * MessageListenerOrderly - 消费端并发且有序消费
 	 * 有序消息的底层实现:
-	 * 	1、producer的保证：单线程顺序发送，将相同orderId的消息发送到broker的同一个队列上；
-	 * 	2、broker端的保证：保证将消息按顺序发送给consumer；
-	 *  3、consumer端的保证：相同orderId的消息，被绑定到一个专门的线程来进行处理；
-	 *  在consumer端通过线程池提供多线程并发处理不同orderId的消息，不会发生消息乱序处理。
+	 * 	1、producer的保证：orderId按队列个数取模，选择将消息推送到某个队列，实现具有相同orderId的消息被放到同一个队列中；
+	 * 	2、broker端的保证：将消息按顺序发送给consumer；
+	 *  3、consumer端的保证：消费线程与队列是具有绑定关系的，一个队列的数据仅能被一个专门的线程来消费。
+	 *  基于以上保证，对于相同orderId的消息，由于分配在了同一个队列，在consumer端又是由单线程处理该队列上的消息，因此，确保了消息的顺序消费。
+	 *  另外，在consumer端通过线程池并发消费消息，实现了不同orderId消息被并发消费，相同orderId的消息被有序消费！
 	 */
 	class Listener implements MessageListenerOrderly {
 
