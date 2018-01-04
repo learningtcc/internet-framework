d9a3e905d7e691f38eedee44193a0a7e4a6f6065
clonegod <asynclife@163.com>
2018-01-05 00:52:00 +0800

    dubbo service config

--- dubbo/dubbo-service-provider/src/main/java/clonegod/dubbo/service/provider/UserServiceProvider_Node2.java
+++ dubbo/dubbo-service-provider/src/main/java/clonegod/dubbo/service/provider/UserServiceProvider_Node2.java
@@ -1,15 +1,15 @@
 package clonegod.dubbo.service.provider;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class UserServiceProvider {
+public class UserServiceProvider_Node2 {
     public static void main(String[] args) throws Exception {
     	
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] {"META-INF/spring/dubbo-service-provider.xml"});
+                new String[] {"META-INF/spring/dubbo-service-provider-node2.xml"});
        
         context.start();
         
-        System.out.println("Dubbo User Service Provider Starting...");
+        System.out.println("Dubbo User Service Provider Starting On Node2...");
         System.out.println("Press Any Key To Exit.");
         
         System.in.read(); // press any key to exit
